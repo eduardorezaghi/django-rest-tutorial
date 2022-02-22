ace188a1c436799061ce29f02e6e6171e4818579
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2022-02-22 11:58:17 -0300

    Add User to views and serializers

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
@@ -11,3 +11,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
