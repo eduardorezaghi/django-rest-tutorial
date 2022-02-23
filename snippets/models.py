128c473179f44afa894cff02cef5cbe28ae151c4
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-23 11:41:51 -0300

    Update Snippet model fields

--- snippets/models.py
+++ snippets/models.py
@@ -13,14 +13,14 @@ class Snippet(models.Model):
     """
     Snippet Model Class
     """
-    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-    highlighted = models.TextField()    
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
@@ -34,6 +34,6 @@ class Snippet(models.Model):
         linenos = 'table' if self.linenos else False
         options = {'title': self.title} if self.title else {}
         formatter = HtmlFormatter(style=self.style, linenos=linenos,
-                                  full=True, **options)
+                                full=True, **options)
         self.highlighted = highlight(self.code, lexer, formatter)
         super().save(*args, **kwargs)
